eacaab6c9d20b606ccdbbe081d3e99c2bf7944ae
laurelcri8 <boldabijuwon360@gmail.com>
2022-04-23 14:46:57 +0100

    remove navlinks onclick

--- javascript/main.js
+++ javascript/main.js
@@ -5,6 +5,10 @@ open.addEventListener('click', ()=>{
     open.classList.toggle('fa-times');
     navlinks.classList.toggle('active');
 })
+navlinks.addEventListener('click', ()=>{
+    open.classList.toggle('fa-bar');
+    navlinks.classList.remove('active');
+})
 
 var text = document.querySelector('.text');
 window.addEventListener('scroll', function(){
